8d1852d9e11f877763a9c31a31e7a1c0e7334d76
mo <opitz@derivo.de>
2022-02-21 17:22:10 +0100

    initiali commit

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # init_semspect
+
+Simple python script that demonstrates how the semspect init can be triggered for different users.
+
+## Use Case
+
+On the first connection of a user to the semspect-neo4j-plugin it has to analyse the current DBMS. Depending on the size
+of the database this initialization process can take seconds, minutes or hours. Note that because of the
+fine-grained-access-control this process has to be done for each user individually. Also, the initialization process has
+to be done after each update of the database.
+
+## How it works
+
+We utilize the impersonate feature that was introduced in neo4j
+4.4 : https://neo4j.com/docs/cypher-manual/current/access-control/dbms-administration/#access-control-dbms-administration-impersonation
+From https://neo4j.com/docs/cypher-manual/current/access-control/dbms-administration/#access-control-dbms-administration-impersonation:
+> The DBMS privileges for impersonation are assignable using Cypher administrative commands.
+> They can be granted, denied, and revoked like other privileges.
+> Impersonation is the capability of a user to assume another user’s roles (and therefore privileges),
+> with the restriction of not being able to execute updating admin commands as the impersonated user
+> (i.e. they would still be able to use SHOW commands). This feature allows an user to run queries
+
+In practice this means that we grant one the user the right to impersonate all semspectUsers that we want to initialize.
+
+## Usage
+
+### Setup Neo4j
+
+1. Create an `initUser` : This will be the user that impersonates the semspect users. 
+`CREATE USER initUser SET PLAINTEXT PASSWORD 'secret_password' CHANGE NOT REQUIRED
+2. Create an `impersonateSemspectUser` role: `CREATE ROLE impersonateSemspectUser`
+3. Grant the `impersonateSemspectUser` role to the `initUser`: `GRANT ROLE impersonateSemspectUser TO initUser`
+4. Grant impersonate privilege for all semspectUsers to the `impersonateSemspectUser` role:
+`GRANT IMPERSONATE ($SEMSPECT_USER) ON DBMS TO impersonateSemspectUser`
+
+### Running the script
+
+```shell
+# We use poetry 
+## to install the dependencies
+poetry install
+##to run the script
+poetry run init_semspect
+```
+
---
+++ init_semspect.py
@@ -0,0 +1,91 @@
+import sys
+
+from neo4j import GraphDatabase, Neo4jDriver
+from dataclasses import dataclass
+import logging
+
+from neo4j.exceptions import Neo4jError
+
+logging.basicConfig()
+logger = logging.getLogger("init_semspect")
+logger.setLevel(level=logging.INFO)
+
+
+@dataclass(frozen=True)
+class Neo4jProcedure:
+    name: str
+    expected_status_code: int
+
+    def create_call(self):
+        return f'CALL {self.name}'
+
+
+@dataclass(frozen=True)
+class Configuration:
+    procedure: Neo4jProcedure
+    user: str
+    database: str
+
+
+# List of semspect procedure definitions
+SEMSPECT_INIT = Neo4jProcedure('semspect.init', 200)
+SEMSPECT_INIT_NO_WRITE = Neo4jProcedure(f'{SEMSPECT_INIT.name}NoWrite', 200)
+SEMSPECT_RELOAD = Neo4jProcedure('semspect.reload', 200)
+SEMSPECT_RELOAD_NO_WRITE = Neo4jProcedure(f'{SEMSPECT_RELOAD.name}NoWrite', 200)
+
+
+def main():
+    configurations = [
+        Configuration(procedure=SEMSPECT_INIT, user="Alice", database="neo4j"),
+        Configuration(procedure=SEMSPECT_INIT_NO_WRITE, user="Bob", database="neo4j")
+    ]
+    if _run_configurations(configurations):
+        sys.exit(0)
+    sys.exit(255)
+
+
+def _run_configurations(configurations):
+    """Run given configurations"""
+    faulty_configurations = []
+    with _create_driver() as driver:
+        for i, configuration in enumerate(configurations):
+            logger.info(f'Running configuration {i + 1}/{len(configurations)}. Configuration: {configuration}')
+            if not _run_configuration(driver, configuration):
+                faulty_configurations.append(configuration)
+
+    if not faulty_configurations:
+        logger.info('All Configurations succeeded')
+        return True
+    else:
+        logger.error(f'There were errors during the execution of following configurations: {faulty_configurations}')
+        return False
+
+
+def _run_configuration(driver: Neo4jDriver, configuration: Configuration):
+    """Run one configuration with given driver"""
+    logger.debug(f'Running configuration {configuration}')
+    with driver.session(database=configuration.database, impersonated_user=configuration.user) as session:
+        _run_procedure(session, configuration.procedure)
+
+
+def _run_procedure(session, procedure):
+    try:
+        record = session.run(procedure.create_call()).single()
+        logger.debug(f'Received answer {record}')
+        status_code = record['status']
+        if status_code != procedure.expected_status_code:
+            logger.error(
+                f'Returned status {status_code} code didn\'t match expected status code {procedure.expected_status_code}. Error: {record["errors"]}')
+            return False
+        return True
+    except Neo4jError as err:
+        logger.error(f'Received neo4j error {err}')
+
+
+def _create_driver() -> Neo4jDriver:
+    """Utility method to create a connection to neo4j"""
+    return GraphDatabase.driver('neo4j://localhost:7687', auth=('initUser', 'b'))
+
+
+if __name__ == '__main__':
+    main()
