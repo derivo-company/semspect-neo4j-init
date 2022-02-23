bc5e2dd6c9a5301d2fdb8ff4bf090d4d627f1126
mo <opitz@derivo.de>
2022-02-23 16:01:42 +0100

    comments

--- init_semspect.py
+++ init_semspect.py
@@ -1,6 +1,7 @@
-import sys
+from builtins import function
+from typing import List
 
-from neo4j import GraphDatabase, Neo4jDriver
+from neo4j import Neo4jDriver
 from dataclasses import dataclass
 import logging
 
@@ -12,7 +13,9 @@ logger.setLevel(level=logging.INFO)
 
 
 @dataclass(frozen=True)
-class Neo4jProcedure:
+class SemspectProcedure:
+    """A Semspect procedure that can be called. Note: A semspect procedure has a name and an expected status code
+    that can be checked """
     name: str
     expected_status_code: int
 
@@ -22,47 +25,36 @@ class Neo4jProcedure:
 
 @dataclass(frozen=True)
 class Configuration:
-    procedure: Neo4jProcedure
+    """What configuration should be called for what user on what database"""
+    procedure: SemspectProcedure
     user: str
     database: str
 
 
-# List of semspect procedure definitions
-SEMSPECT_INIT = Neo4jProcedure('semspect.init', 200)
-SEMSPECT_INIT_NO_WRITE = Neo4jProcedure(f'{SEMSPECT_INIT.name}NoWrite', 200)
-SEMSPECT_RELOAD = Neo4jProcedure('semspect.reload', 200)
-SEMSPECT_RELOAD_NO_WRITE = Neo4jProcedure(f'{SEMSPECT_RELOAD.name}NoWrite', 200)
+class SemspectProcedures:
+    """Predefined list of semspect procedures"""
+    SEMSPECT_INIT = SemspectProcedure('semspect.init', 200)
+    SEMSPECT_INIT_NO_WRITE = SemspectProcedure(f'{SEMSPECT_INIT.name}NoWrite', 200)
+    SEMSPECT_RELOAD = SemspectProcedure('semspect.reload', 200)
+    SEMSPECT_RELOAD_NO_WRITE = SemspectProcedure(f'{SEMSPECT_RELOAD.name}NoWrite', 200)
 
 
-def main():
-    configurations = [
-        Configuration(procedure=SEMSPECT_INIT, user="Alice", database="neo4j"),
-        Configuration(procedure=SEMSPECT_INIT_NO_WRITE, user="Bob", database="neo4j")
-    ]
-    if _run_configurations(configurations):
-        sys.exit(0)
-    sys.exit(255)
-
-
-def _run_configurations(configurations):
-    """Run given configurations"""
+def run_configurations(driver_factory: function, configurations: List[Configuration]):
+    """Run given configurations with given driver
+    :param driver_factory : driver_factory returning a Neo4jDriver
+    :param configurations: List of configurations to run
+    :returns a list of configurations that weren't executed successfully
+     """
     faulty_configurations = []
-    with _create_driver() as driver:
+    with driver_factory() as driver:
         for i, configuration in enumerate(configurations):
             logger.info(f'Running configuration {i + 1}/{len(configurations)}. Configuration: {configuration}')
             if not _run_configuration(driver, configuration):
                 faulty_configurations.append(configuration)
-
-    if not faulty_configurations:
-        logger.info('All Configurations succeeded')
-        return True
-    else:
-        logger.error(f'There were {len(faulty_configurations)} errors during the execution of following '
-                     f'configurations: {faulty_configurations}')
-        return False
+    return faulty_configurations
 
 
-def _run_configuration(driver: Neo4jDriver, configuration: Configuration):
+def _run_configuration(driver, configuration):
     """Run one configuration with given driver"""
     logger.debug(f'Running configuration {configuration}')
     with driver.session(database=configuration.database, impersonated_user=configuration.user) as session:
@@ -83,12 +75,3 @@ def _run_procedure(session, procedure):
     except Neo4jError as err:
         logger.error(f'Received neo4j error {err}')
         return False
-
-
-def _create_driver() -> Neo4jDriver:
-    """Utility method to create a connection to neo4j"""
-    return GraphDatabase.driver('neo4j://localhost:7687', auth=('initUser', 'b'))
-
-
-if __name__ == '__main__':
-    main()
