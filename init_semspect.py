878eeea6b68e03b50845fb74f639cafca496e4b5
mo <opitz@derivo.de>
2022-03-28 16:49:32 +0200

    implicit call of reload functions

--- init_semspect.py
+++ init_semspect.py
@@ -9,6 +9,16 @@ logger.setLevel(level=logging.INFO)
 
 
 @dataclass(frozen=True)
+class UserType:
+    type: str
+
+
+class UserTypes:
+    READ_ONLY = UserType("READ_ONLY")
+    READ_WRITE = UserType("READ_WRITE")
+
+
+@dataclass(frozen=True)
 class SemspectProcedure:
     """A Semspect procedure that can be called. Note: A semspect procedure has a name and an expected status code
     that can be checked """
@@ -22,7 +32,7 @@ class SemspectProcedure:
 @dataclass(frozen=True)
 class Configuration:
     """What configuration should be called for what user on what database"""
-    procedure: SemspectProcedure
+    user_type: UserType
     user: str
     database: str
 
@@ -54,7 +64,28 @@ def _run_configuration(driver, configuration):
     """Run one configuration with given driver"""
     logger.debug(f'Running configuration {configuration}')
     with driver.session(database=configuration.database, impersonated_user=configuration.user) as session:
-        return _run_procedure(session, configuration.procedure)
+        reload_procedure, init_procedure = _user_type_to_semspect_procedures(configuration.user_type)
+        if _run_procedure(session, reload_procedure):
+            # reload worked (Status code 200).
+            # This covers two cases: Either there was no reload necessary because the data didn't change since
+            # last initialization or reload worked.
+            return True
+        if _run_procedure(session, init_procedure):
+            # init worked (Status code 200):
+            # This covers two cases: init was performed, or init was not necessary
+            return True
+        # both (reload and init) failed for some reason.
+        return False
+
+
+def _user_type_to_semspect_procedures(user_type):
+    """Maps an UserType to one reload and one init function"""
+    if user_type == UserTypes.READ_WRITE:
+        return SemspectProcedures.SEMSPECT_RELOAD, SemspectProcedures.SEMSPECT_INIT
+    if user_type == UserTypes.READ_ONLY:
+        return SemspectProcedures.SEMSPECT_RELOAD_NO_WRITE, SemspectProcedures.SEMSPECT_INIT_NO_WRITE
+    else:
+        raise RuntimeError(f'Unknown user type {user_type}')
 
 
 def _run_procedure(session, procedure):
@@ -63,11 +94,11 @@ def _run_procedure(session, procedure):
         logger.debug(f'Received answer {record}')
         status_code = record['status']
         if status_code != procedure.expected_status_code:
-            logger.error(
+            logger.debug(
                 f'Returned status {status_code} code did not match expected {procedure.expected_status_code}.'
                 f'Error: {record["errors"]}')
             return False
         return True
-    except (Neo4jError,DriverError) as err:
+    except (Neo4jError, DriverError) as err:
         logger.error(f'Received neo4j error {err}')
         return False
